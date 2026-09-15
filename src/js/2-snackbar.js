44a460f774ecaf8ec3c80c7aa131fdf569b3500c
vilana <beobeo257@gmail.com>
2026-09-16 00:46:47 +0300

    ver1

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,44 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { delay, state } = event.currentTarget.elements;
+  const delayValue = Number(delay.value);
+  const stateValue = state.value;
+
+  createPromise(delayValue, stateValue)
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
